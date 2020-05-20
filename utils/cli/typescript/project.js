7406763dcd1b1b9e9f6cf7baf6e44e51b6715f8a
Nguyễn Đức Long <longnd@rabiloo.com.vn>
2020-05-20 11:33:57 +0700

    update create project

--- utils/cli/typescript/project.js
+++ utils/cli/typescript/project.js
@@ -1,19 +1,20 @@
 const Path = require("path");
 const { execSync } = require("child_process");
 const EXAMPLE_PROJECT =
-    "git@github.com:nguyenduclong-ict/express-typescript.git";
+    "https://github.com/nguyenduclong-ict/express-typescript.git";
 
 module.exports.newProject = async function (args = []) {
     const projectName = args[0]; // project name
-    console.log("Clone project");
     if (!projectName) {
         console.error("Missing project name");
         return;
     }
+    console.log("Clone project ...");
     execSync(
         "git clone " +
             EXAMPLE_PROJECT +
             " " +
-            Path.join(process.cwd(), projectName)
+            Path.join(process.cwd(), projectName) +
+            ` cd ${projectName} && yarn`
     );
 };
