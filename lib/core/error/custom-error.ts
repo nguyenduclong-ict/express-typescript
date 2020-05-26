07c30af38408e752328d7c0c63040a78d47f0841
Nguyễn Đức Long <longnd@rabiloo.com.vn>
2020-05-26 14:13:45 +0700

    update

--- lib/core/error/custom-error.ts
+++
@@ -1,8 +0,0 @@
-export default class CustomError extends Error {
-    code: number
-
-    constructor({ message, code }: { message?: string; code?: number }) {
-        super(message)
-        this.code = code
-    }
-}
